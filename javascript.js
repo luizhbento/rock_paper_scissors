4861f74304bf9581a1e92ef0dedb73636bb6fe7a
Luiz Bento <32204385+luizhbento@users.noreply.github.com>
2024-02-04 15:45:45 -0300

    Updating work done

--- javascript.js
+++ javascript.js
@@ -1,50 +1,64 @@
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 
-buttons.forEach((button) =>{
-    button.addEventListener('click', () =>{
-        playRound(button.id);
-    });
+let humanScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (humanScore + computerScore === 0) {
+      const results = document.querySelector("#results");
+      const content = document.createElement("h4");
+    }
+
+    if (humanScore <     5 && computerScore < 5) {
+      content = document.createElement("div");
+      content.classList.add("result");
+      content.textContent = playRound(button.id);
+      results.appendChild(content);
+    } else {
+    }
+  });
 });
 
-function playRound(playerSelection){
-    let computerPlay = getComputerChoice();
+function playRound(playerSelection) {
+  let computerPlay = getComputerChoice();
 
-    if(playerSelection === "Rock"){
-        if(computerPlay === "Rock"){
-            console.log("It`s a Draw!");
-        }else if(computerPlay === "Paper"){
-            console.log("You lose!");
-        }else{
-            console.log("You win!");
-        }
-    }else if(playerSelection === "Paper"){
-        if(computerPlay === "Rock"){
-            console.log("You win!");
-        }else if(computerPlay === "Paper"){
-            console.log("It`s a Draw!");
-        }else{
-            console.log("You lose!");
-        }
-    }else if(playerSelection === "Scissors"){
-        if(computerPlay === "Rock"){
-            console.log("You lose!");
-        }else if(computerPlay === "Paper"){
-            console.log("You win!");
-        }else{
-            console.log("It`s a Draw!");
-        }
-    }else{
-        console.log("You did not wrote any of the correct options... :(");
+  if (playerSelection === "Rock") {
+    if (computerPlay === "Rock") {
+      return "It`s a Draw!";
+    } else if (computerPlay === "Paper") {
+      return "You lose!";
+    } else {
+      return "You win!";
     }
+  } else if (playerSelection === "Paper") {
+    if (computerPlay === "Rock") {
+      return "You win!";
+    } else if (computerPlay === "Paper") {
+      return "It`s a Draw!";
+    } else {
+      return "You lose!";
+    }
+  } else if (playerSelection === "Scissors") {
+    if (computerPlay === "Rock") {
+      return "You lose!";
+    } else if (computerPlay === "Paper") {
+      return "You win!";
+    } else {
+      return "It`s a Draw!";
+    }
+  } else {
+    return "You did not wrote any of the correct options... :(";
+  }
 }
 
-function getComputerChoice(){
-    let seed = Math.random();
-    if (seed <= 1/3){
-        return "Rock";
-    }else if(seed <= 2/3){
-        return "Paper";
-    }else{
-        return "Scissors";
-    }
+function getComputerChoice() {
+  let seed = Math.random();
+  if (seed <= 1 / 3) {
+    return "Rock";
+  } else if (seed <= 2 / 3) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
 }
